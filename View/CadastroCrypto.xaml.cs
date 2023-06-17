d4588442fbfd7424806e313a46bcb4d1f7142cd0
MarcusOliveiraThomazOliveira <mvthomazoliveira@gmail.com>
2023-06-17 16:28:47 -0300

    Atualização do cadastro.

---
+++ View/CadastroCrypto.xaml.cs
@@ -0,0 +1,18 @@
+using DeZooiNaCrypto.Model;
+
+namespace DeZooiNaCrypto.View;
+
+public partial class CadastroCrypto : ContentPage
+{
+    private Usuario _usuario;
+    public CadastroCrypto(Usuario usuario)
+    {
+        _usuario = usuario;
+        InitializeComponent();
+    }
+
+    private void Gravar(object sender, EventArgs e)
+    {
+
+    }
+}
--- View/ListarCryptos.xaml.cs
+++ View/ListarCryptos.xaml.cs
@@ -4,10 +4,15 @@ namespace DeZooiNaCrypto.View;
 
 public partial class ListarCryptos : ContentPage
 {
-	private Usuario _usuario;
-	public ListarCryptos(Usuario usuario)
-	{
-		_usuario = usuario;
-		InitializeComponent();
-	}
+    private Usuario _usuario;
+    public ListarCryptos(Usuario usuario)
+    {
+        _usuario = usuario;
+        InitializeComponent();
+    }
+
+    private void CadastrarCrypto(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new CadastroCrypto(_usuario));
+    }
 }
